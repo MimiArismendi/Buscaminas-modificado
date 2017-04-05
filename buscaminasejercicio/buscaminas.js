d3db872026612887fda8579bb843e999800f928e
noemi <noemiarismendi.cc@gmail.com>
2017-04-05 10:40:20 -0500

    segunda version

---
+++ buscaminasejercicio/buscaminas.js
@@ -0,0 +1,13 @@
+/*function mostrar(){
+document.getElementById('oculto2').style.display = 'block';
+}
+
+function ocultar(){
+document.getElementById("oculto").style.display = 'none';
+}*/
+
+function mostrarImagen(id){
+  var imagenes = document.getElementById("imagen");
+  imagenes.innerHTML = '<img src = "boom.jpg"/>'
+
+}
